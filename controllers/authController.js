74ab088f1626d4eeb36e32a7acb55350e053b43f
philipthedeveloper <philipowolabi79@gmail.com>
2023-10-06 12:31:57 +0100

    Refactor authController to use new helpers and custom functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,16 +6,23 @@ import {
   createUnprocessableEntityError,
 } from "../errors/index.js";
 import _ from "lodash";
+import {
+  sendSuccessResponse,
+  throwRequestError,
+  checkEmptyRequestBody,
+} from "../helpers/index.js";
 import sendEmail from "../helpers/email-sender.js";
+import * as ERROR_TYPES from "../helpers/errorTypes.js";
 
 const login = async function (req, res) {
   const data = req.body;
-  const numOfKeys = Object.keys(data).length;
-  if (numOfKeys === 0 || !data.email || !data.password)
-    throw createBadRequestError("Email and Password required");
+  const isBodyEmpty = checkEmptyRequestBody(data);
+  if (isBodyEmpty || !data.email || !data.password)
+    throwRequestError(ERROR_TYPES.BAD_REQUEST, "Email and Password required");
 
   const user = await User.findOne({ email: data.email });
-  if (!user) throw createUnauthorizedError("Invalid Email or Password");
+  if (!user)
+    throwRequestError(ERROR_TYPES.UNAUTHORIZED, "Invalid Email or Password");
   // Validate the password using the document method (validatePassword)
   /**
    * @function
@@ -24,15 +31,13 @@ const login = async function (req, res) {
    */
   const isValidPassword = await user.validatePassword(data.password);
   if (!isValidPassword)
-    throw createUnauthorizedError("Invalid Email or Password");
+    throwRequestError(ERROR_TYPES.UNAUTHORIZED, "Invalid Email or Password");
   // Generate jwt for valid email and password
   const userWithoutPassword = _.omit(user.toObject(), "password");
   userWithoutPassword.userId = userWithoutPassword._id;
   const { userId, email, fullname } = userWithoutPassword;
   const accessToken = user.generateToken({ userId, email, fullname });
-  return res.status(StatusCodes.OK).json({
-    success: true,
-    status: StatusCodes.OK,
+  return sendSuccessResponse(res, {
     message: "Login successful",
     user: userWithoutPassword,
     accessToken,
@@ -41,44 +46,48 @@ const login = async function (req, res) {
 
 const register = async function (req, res) {
   const data = req.body;
-  const numOfKeys = Object.keys(data).length;
-  if (numOfKeys === 0) throw createBadRequestError("Provide all fields");
+  const isBodyEmpty = checkEmptyRequestBody(data);
+  if (isBodyEmpty)
+    throwRequestError(ERROR_TYPES.BAD_REQUEST, "Provide all fields");
   const newUser = await User.create(data);
   const userWithoutPassword = _.omit(newUser.toObject(), "password");
   userWithoutPassword.userId = userWithoutPassword._id;
   const { userId, email, fullname } = userWithoutPassword;
   const accessToken = newUser.generateToken({ userId, email, fullname });
-  return res.status(StatusCodes.CREATED).json({
-    success: true,
-    status: StatusCodes.CREATED,
-    message: "Account created successfully",
-    user: userWithoutPassword,
-    accessToken,
-  });
+  return sendSuccessResponse(
+    res,
+    {
+      message: "Account created successfully",
+      user: userWithoutPassword,
+      accessToken,
+    },
+    StatusCodes.CREATED
+  );
 };
 
 const getUser = async (req, res) => {
   if (req.currentUser) {
     const userWithoutPassword = _.omit(req.currentUser.toObject(), "password");
     userWithoutPassword.userId = userWithoutPassword._id;
-    return res.status(StatusCodes.OK).json({
-      success: true,
-      status: StatusCodes.OK,
+    return sendSuccessResponse(res, {
       message: "Successful",
       user: userWithoutPassword,
     });
   }
-  throw createUnauthorizedError("User not authenticated");
+  throwRequestError(ERROR_TYPES.UNAUTHORIZED, "User not authenticated");
 };
 
 const emailVerification = async (req, res) => {
   const { email } = req.currentUser;
   if (req.currentUser.eligibility === "Allowed")
-    throw createUnprocessableEntityError("Account already verified");
+    throwRequestError(
+      ERROR_TYPES.UNPROCESSABLE_ENTITY,
+      "Account already verified"
+    );
   const user = await User.findOne({
     email: { $regex: email, $options: "i" },
   });
-  if (!user) throw createUnauthorizedError("Unauthorized User");
+  if (!user) throwRequestError(ERROR_TYPES.UNAUTHORIZED, "Unauthorized User");
   const otpCode = Math.floor(Math.random() * 900000) + 100000;
   const expiresIn = new Date().getTime() + 300 * 1000;
   const newOtpDoc = { email, otpCode: otpCode.toString(), expiresIn };
@@ -90,31 +99,39 @@ const emailVerification = async (req, res) => {
     const otp = await Otp.create(newOtpDoc);
   }
   await sendEmail({ otpCode, email });
-  res.status(StatusCodes.OK).json({
-    success: true,
-    status: StatusCodes.OK,
-    msg: "A verification code has been sent.",
+  return sendSuccessResponse(res, {
+    message: "A verification code has been sent to your email.",
   });
 };
 
 const verifyOTP = async (req, res) => {
   const { email } = req.currentUser;
-  if (!email) throw createUnauthorizedError("Unauthorized user");
+  if (!email) throwRequestError(ERROR_TYPES.UNAUTHORIZED, "Unauthorized user");
   if (req.currentUser.eligibility === "Allowed")
-    throw createUnprocessableEntityError("Account already verified");
+    throwRequestError(
+      ERROR_TYPES.UNPROCESSABLE_ENTITY,
+      "Account already verified"
+    );
   const { otp } = req.body;
-  if (!otp) throw createBadRequestError("Must provide verification code");
-  if (otp.length < 6) throw createUnprocessableEntityError("Invalid token");
+  if (!otp)
+    throwRequestError(
+      ERROR_TYPES.BAD_REQUEST,
+      "Must provide verification code"
+    );
+  if (otp.length < 6)
+    throwRequestError(ERROR_TYPES.UNPROCESSABLE_ENTITY, "Invalid token");
   const token = await Otp.findOne({ email });
   if (!token)
-    throw createUnprocessableEntityError(
+    throwRequestError(
+      ERROR_TYPES.UNPROCESSABLE_ENTITY,
       "Verification failed. Please try again"
     );
   const { otpCode, expiresIn, tries } = token;
   const timeDifference = expiresIn - new Date().getTime();
   if (timeDifference < 0) {
     await Otp.findOneAndDelete({ email });
-    throw createBadRequestError(
+    throwRequestError(
+      ERROR_TYPES.BAD_REQUEST,
       "Token already expired. Please request a new one"
     );
   }
@@ -123,21 +140,18 @@ const verifyOTP = async (req, res) => {
     if (tries < 2) {
       await token.$inc("tries", 1);
       await token.save();
-      throw createUnprocessableEntityError("Invalid token");
+      throwRequestError(ERROR_TYPES.UNPROCESSABLE_ENTITY, "Invalid token");
     }
     await Otp.findOneAndDelete({ email });
-    throw createUnprocessableEntityError(
+    throwRequestError(
+      ERROR_TYPES.UNPROCESSABLE_ENTITY,
       "Invalid Token. Maximum tries exceed. Please request a new token."
     );
   }
   await Otp.findOneAndDelete({ email });
   await req.currentUser.$set("eligibility", "Allowed");
   await req.currentUser.save();
-  return res.status(StatusCodes.OK).json({
-    msg: "Verification successful",
-    success: true,
-    status: StatusCodes.OK,
-  });
+  return sendSuccessResponse(res, { message: "Verification successful" });
 };
 
 export { login, register, getUser, emailVerification, verifyOTP };
