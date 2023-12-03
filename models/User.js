dbae244911055d01c1e838196454e9a7b9536e83
Philip Owolabi <philipowolabi79@gmail.com>
2023-12-03 02:59:34 +0100

    Hot fix: Fix password rehashing
    Update the user model middlewares to prevent password rehashing on save

--- models/User.js
+++ models/User.js
@@ -65,7 +65,12 @@ const UserSchema = new mongoose.Schema(
 );
 
 // Setup bcryptjs for password hashing
-UserSchema.pre("save", async function () {
+UserSchema.pre("save", async function (next) {
+  // Check if the password field is modified
+  if (!this.isModified("password")) {
+    return next();
+  }
+
   let salt = await bcryptjs.genSalt(10);
   this.password = await bcryptjs.hash(this.password, salt);
 });
