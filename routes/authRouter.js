712c7955e4520cc7b1b1bf83451b91212547808c
Philip Owolabi <philipowolabi79@gmail.com>
2023-12-03 02:58:41 +0100

    add new endpoints for revoking and removing account

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,6 +4,8 @@ import {
   register,
   emailVerification,
   verifyOTP,
+  revokeUserEligibility,
+  removeAccount,
 } from "../controllers/authController.js";
 import { Router } from "express";
 import validateToken from "../middlewares/validate-jwt.js";
@@ -17,6 +19,8 @@ authRouter.post("/register", register);
 authRouter.post("/get-user", validateToken, getUser);
 authRouter.post("/email-verification", validateToken, emailVerification);
 authRouter.post("/verify-otp", validateToken, verifyOTP);
+authRouter.post("/revoke-access", validateToken, revokeUserEligibility);
+authRouter.post("/remove-account", validateToken, removeAccount);
 
 // Export the router
 export default authRouter;
